b355d299f5b5883a099513b457a2d0e101cf15eb
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-04-05 09:51:09 -0400

    Added composition API

--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -2,11 +2,17 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
 import { ref, onMounted } from 'vue'
-  
-
+const pokemon = ref('')
+async function getPokemon() {
+  let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+  let data = await res.json()
+  pokemon.value = data.results
+}
+onMounted(() => {
+  getPokemon()
+})
 </script>
 
-
-<style lang="scss" scoped></style>
+<style scoped></style>
