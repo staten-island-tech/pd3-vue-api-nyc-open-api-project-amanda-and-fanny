df0db352033d10e9fcd1a58f1a5e5aab35b88eb8
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2023-04-19 10:14:57 -0400

    Creatting the card

---
+++ Vue/src/components/AnimalCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="card">
+    <h2>{{}}</h2>
+  </div>
+</template>
+
+<style scoped></style>
--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -2,11 +2,27 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
 import { ref, onMounted } from 'vue'
-  
-
+const animals = ref('')
+async function getAnimals() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
+  let data = await res.json()
+  animals.value = data.results
+}
+onMounted(() => {
+  getAnimals()
+})
 </script>
 
-
-<style lang="scss" scoped></style>
+<style scoped>
+.container {
+  width: 80vw;
+  margin: 30px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
