fc1f584585a11b78f981a0dc82c0bdc12ebe01de
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2023-04-20 10:16:16 -0400

    help

--- Vue/src/components/AnimalCard.vue
+++ Vue/src/components/AnimalCard.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="card">
-    <h2>{{}}</h2>
+    <h2>{{ animals.animal_calss }}</h2>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  animals: Object
+})
+</script>
 
 <style scoped></style>
--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <template>
-  <div></div>
+  <div class="container">
+    <AnimalCard v-for="animal in animals" :key="animal.animal_class" :animals="animal" />
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import AnimalCard from '../components/AnimalCard.vue'
+
 const animals = ref('')
 async function getAnimals() {
   let res = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
   let data = await res.json()
   animals.value = data.results
+  console.log(data)
 }
 onMounted(() => {
   getAnimals()
