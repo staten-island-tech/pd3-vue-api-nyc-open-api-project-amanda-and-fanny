072558752ed22d927be2cd1974a211065e808a10
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-04-24 10:13:19 -0400

    8:05

--- Vue/src/components/PokemonCard.vue
+++ Vue/src/components/PokemonCard.vue
@@ -1,16 +1,21 @@
 <template>
-  <router-link :to="" class="card">
+  <router-link :to="pkmnPath" class="card">
     <h2>{{ pokemon.name }}</h2>
     <h3>{{ id }}</h3>
   </router-link>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 const props = defineProps({
   pokemon: Object,
   id: Number
 })
+const pkmnPath = computed(() => {
+  return '/PokemonData/${props.pokemon.name}'
+})
 </script>
+
 <style scoped>
 .card {
   width: 28%;
--- Vue/src/views/PokemonData.vue
+++ Vue/src/views/PokemonData.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <h1>TEST</h1>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      monster: {}
+    }
+  },
+  mounted: async function(){
+    await getPokemon()
+  },
+  methods:{
+    getPokemon(){
+      async function(){
+        let res = await fetch('https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}')
+      }
+    }
+  }
+}
 </script>
 
 <style scoped></style>
