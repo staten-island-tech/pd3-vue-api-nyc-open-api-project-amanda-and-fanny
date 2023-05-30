63b2d87c5ce68416c59e0f31f9d199eeb57a29ef
fannyl6720 <fanny.liang23@sitechhs.com>
2023-05-30 17:41:48 -0400

    fhshsh

--- openData/src/components/BronxViewGraph.vue
+++ openData/src/components/BronxViewGraph.vue
@@ -12,9 +12,17 @@
 </template>
 
 <script>
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
 
-import { Pie } from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
